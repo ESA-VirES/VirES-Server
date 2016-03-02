4b585673e2a9d91ce4638349b819d17d08e7f9de
Daniel Santillan <daniel.santillan@eox.at>
2016-03-02 16:17:58 +0100

    Added templates to load client with different configurations depending if user is authenticated or not; Added an ows_wrapper view to protect ows services;

--- vires/views.py
+++ vires/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from allauth.account.forms import LoginForm, SignupForm
+from eoxserver.services.views import ows
 
-# Create your views here.
+def workspace(request):
+
+    # TODO: check if request.metho is set to "POST"
+    # if yes then login or signup user then do redirect or whatever
+    return render(request, "vires/workspace.html", {
+        "login_form": LoginForm(),
+        "signup_form": SignupForm()
+    })
+
+
+@login_required
+def wrapped_ows(request):
+    return ows(request)
