aaa78f19d32d97c18c5c2d205224538db48d7610
Martin Paces <martin.paces@eox.at>
2016-03-22 13:39:21 +0100

    Reworking the Kp and Dst index handling.

--- vires/vires/cdf_util.py
+++ vires/vires/cdf_util.py
@@ -29,7 +29,21 @@
 #-------------------------------------------------------------------------------
 
 from os.path import exists
+from math import ceil, floor
+from numpy import empty, nan, max as amax, min as amin
+import scipy
+from scipy.interpolate import interp1d
 from spacepy import pycdf
+
+try:
+    from numpy import full
+except ImportError:
+    def full(shape, value, dtype=None, order='C'):
+        """ Numpy < 1.8 workaround. """
+        arr = empty(shape, dtype, order)
+        arr.fill(value)
+        return arr
+
 
 def cdf_open(filename, mode="r"):
     """ Open a new or an existing  CDF file.
@@ -47,3 +61,64 @@ def cdf_open(filename, mode="r"):
     else:
         raise ValueError("Invalid mode value %r!" % mode)
     return cdf
+
+
+def cdf_time_subset(cdf, start, stop, fields, margin=0):
+    """ Extract subset of the listed `fields` from a CDF data file.
+    The extracted range of values match times which lie within the given
+    closed time interval. The time interval is defined by the MDJ2000 `start`
+    and `stop` values.
+    The `margin` parameter is used to extend the index range by N surrounding
+    elements. Negative margin is allowed.
+    """
+    time = cdf['time']
+    idx_start, idx_stop = 0, time.shape[0]
+
+    if start > stop:
+        start, stop = stop, start
+
+    if time.shape[0] > 0:
+        start = max(start, time[0])
+        stop = min(stop, time[-1])
+        time_span = time[-1] - time[0]
+        if time_span > 0:
+            resolution = (time.shape[0] - 1) / time_span
+            idx_start = int(ceil((start - time[0]) * resolution))
+            idx_stop = max(0, 1 + int(floor((stop - time[0]) * resolution)))
+        elif start > time[-1] or stop < time[0]:
+            idx_start = idx_stop # empty selection
+
+    if margin != 0:
+        if idx_start < time.shape[0]:
+            idx_start = max(0, idx_start - margin)
+        if idx_stop > 0:
+            idx_stop = max(0, idx_stop + margin)
+
+    return [(field, cdf[field][idx_start:idx_stop]) for field in fields]
+
+
+def cdf_time_interp(cdf, time, fields, min_len=2, **interp1d_prm):
+    """ Read values of the listed fields from the CDF file and interpolate
+    them at the given time values (the `time` array of MDJ2000 values).
+    The data exceeding the time interval of the source data is filled with the
+    `fill_value`. The function accepts additional keyword arguments which are
+    passed to the `scipy.interpolate.interp1d` interpolation (such as `kind`
+    and `fill_value`).
+    """
+    # additional interpolation parameters
+    if scipy.__version__ >= '0.14':
+        interp1d_prm['assume_sorted'] = True
+    interp1d_prm['copy'] = False
+    interp1d_prm['bounds_error'] = False
+
+    # check minimal length required by the chosen kind of interpolation
+    if time.size > 0 and cdf['time'].shape[0] > min_len:
+        return [
+            (field, interp1d(cdf["time"], cdf[field], **interp1d_prm)(time))
+            for field in fields
+        ]
+    else:
+        return [
+            (field, full(time.shape, interp1d_prm.get("fill_value", nan)))
+            for field in fields
+        ]
---
+++ vires/vires/tests/cdf_util.py
@@ -0,0 +1,94 @@
+#-------------------------------------------------------------------------------
+#
+# Testing Auxiliary Data Handling
+#
+# Project: VirES
+# Authors: Martin Paces <martin.paces@eox.at>
+#
+#-------------------------------------------------------------------------------
+# Copyright (C) 2016 EOX IT Services GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+# pylint: disable=missing-docstring
+
+# TODO: Test cdf_time_interp .
+import unittest
+from contextlib import closing
+from StringIO import StringIO
+
+from vires.cdf_util import cdf_open, cdf_time_subset, cdf_time_interp
+from vires.aux import parse_dst
+from vires.tests.aux import TEST_DST, DATA_DST, ArrayMixIn
+
+class TestCDF(ArrayMixIn, unittest.TestCase):
+    FILE = "./test_tmp_cdf.cdf"
+
+    def setUp(self):
+        with closing(cdf_open(self.FILE, "w")) as cdf:
+            cdf["time"], cdf["dst"], cdf["est"], cdf["ist"], cdf["flag"] = (
+                parse_dst(StringIO(TEST_DST))
+            )
+
+    def _cdf_time_subset(self, fields, start, stop, idx_start, idx_stop, margin=0):
+        """ Testing CDF read. """
+        with cdf_open(self.FILE) as cdf:
+            for field, value in cdf_time_subset(cdf, start, stop, fields, margin):
+                self.assertEqual(value.shape, (idx_stop - idx_start,))
+                self.assertAllEqual(value, cdf[field][idx_start:idx_stop])
+                self.assertAllEqual(value, DATA_DST[field][idx_start:idx_stop])
+
+    def test_cdf_time_subset_margin1(self):
+        # full interval
+        self._cdf_time_subset(('time', 'dst'), -729.97917, -728.56250, 0, 35, 1)
+        self._cdf_time_subset(('time', 'dst'), -731.00, -728.00, 0, 35, 1)
+        self._cdf_time_subset(('time', 'dst'), -729.97, -728.57, 0, 35, 1)
+        # inner subset
+        self._cdf_time_subset(('time', 'dst'), -729.60, -729.00, 9, 25, 1)
+        # partial overlap - lower
+        self._cdf_time_subset(('time', 'dst'), -733.00, -729.00, 0, 25, 1)
+        # partial overlap - upper
+        self._cdf_time_subset(('time', 'dst'), -729.60, -725.00, 9, 35, 1)
+        # no overlap - lower
+        self._cdf_time_subset(('time', 'dst'), -733.00, -730.00, 0, 0, 1)
+        self._cdf_time_subset(('time', 'dst'), -733.00, -732.00, 0, 0, 1)
+        # no overlap - upper
+        self._cdf_time_subset(('time', 'dst'), -728.54, -725.00, 0, 0, 1)
+        self._cdf_time_subset(('time', 'dst'), -724.00, -725.00, 0, 0, 1)
+
+    def test_cdf_time_subset(self):
+        # full interval
+        self._cdf_time_subset(('time', 'dst'), -729.97917, -728.56250, 0, 35)
+        self._cdf_time_subset(('time', 'dst'), -731.00, -728.00, 0, 35)
+        # inner subset
+        self._cdf_time_subset(('time', 'dst'), -729.60, -729.00, 10, 24)
+        # partial overlap - lower
+        self._cdf_time_subset(('time', 'dst'), -733.00, -729.00, 0, 24)
+        # partial overlap - upper
+        self._cdf_time_subset(('time', 'dst'), -729.60, -725.00, 10, 35)
+        # no overlap - lower
+        self._cdf_time_subset(('time', 'dst'), -733.00, -730.00, 0, 0)
+        self._cdf_time_subset(('time', 'dst'), -733.00, -732.00, 0, 0)
+        # no overlap - upper
+        self._cdf_time_subset(('time', 'dst'), -728.54, -725.00, 0, 0)
+        self._cdf_time_subset(('time', 'dst'), -724.00, -725.00, 0, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
