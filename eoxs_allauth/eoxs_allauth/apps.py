f891074da2a1fb24a903c5ab25a8b0ee9b9e2fb5
Daniel Santillan <daniel.santillan@eox.at>
2016-04-05 10:19:55 +0200

    Added app and signals to add profile object to already available users in database during migration; Added license header to files

--- eoxs_allauth/eoxs_allauth/apps.py
+++ eoxs_allauth/eoxs_allauth/apps.py
@@ -25,17 +25,11 @@
 # THE SOFTWARE.
 #-------------------------------------------------------------------------------
 
+from django.apps import AppConfig
 
+class TasksConfig(AppConfig):
+    name = 'tasks'
+    verbose_name = "Tasks"
 
-from django.db import models
-from django.contrib.auth.models import User
-from django_countries.fields import CountryField
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    title = models.CharField(max_length=100, blank=True)
-    institution = models.CharField(max_length=100, blank=True)
-    country = CountryField(blank=True, blank_label='(select country)')
-    study_area = models.CharField(max_length=200, blank=True)
-    executive_summary = models.CharField(max_length=3000, blank=True)
+    def ready(self):
+        import eoxs_allauth.eoxs_allauth.signals.handlers #noqa
--- eoxs_allauth/eoxs_allauth/signals/handlers.py
+++ eoxs_allauth/eoxs_allauth/signals/handlers.py
@@ -26,16 +26,13 @@
 #-------------------------------------------------------------------------------
 
 
+from django.dispatch import receiver
+from eoxs_allauth.models import UserProfile
+from south.signals import post_migrate
 
-from django.db import models
-from django.contrib.auth.models import User
-from django_countries.fields import CountryField
 
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    title = models.CharField(max_length=100, blank=True)
-    institution = models.CharField(max_length=100, blank=True)
-    country = CountryField(blank=True, blank_label='(select country)')
-    study_area = models.CharField(max_length=200, blank=True)
-    executive_summary = models.CharField(max_length=3000, blank=True)
+@receiver(post_migrate)
+def create_profiles(app, **kwargs):
+    if app == "eoxs_allauth":
+        for u in User.objects.all():
+            UserProfile.objects.get_or_create(user=u)
