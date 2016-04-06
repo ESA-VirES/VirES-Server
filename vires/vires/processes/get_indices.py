c5d7dd9c4e48897c10e416ae9f6c28b9a2888119
Martin Paces <martin.paces@eox.at>
2016-04-06 09:27:07 +0200

    Thorough review of the get_indices process.

--- vires/vires/processes/get_indices.py
+++ vires/vires/processes/get_indices.py
@@ -24,130 +24,116 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 #-------------------------------------------------------------------------------
-import math
-import csv
-import datetime as dt
-from itertools import izip
-from itertools import izip_longest
-import time
-from StringIO import StringIO
-try:
-    # available in Python 2.7+
-    from collections import OrderedDict
-except ImportError:
-    from django.utils.datastructures import SortedDict as OrderedDict
-import numpy as np
+# pylint: disable=missing-docstring, too-many-locals, too-few-public-methods
 
+from datetime import datetime
+from itertools import izip
+from cStringIO import StringIO
+from numpy import amax, amin, vectorize, choose
 from django.conf import settings
 from eoxserver.core import Component, implements
 from eoxserver.services.ows.wps.interfaces import ProcessInterface
-from eoxserver.services.ows.wps.exceptions import InvalidOutputDefError
-from eoxserver.services.result import ResultBuffer, ResultFile
 from eoxserver.services.ows.wps.parameters import (
-    ComplexData, CDObject, CDTextBuffer,
-    FormatText, FormatXML, FormatJSON, #FormatBinaryRaw, FormatBinaryBase64,
-    BoundingBoxData, BoundingBox,
-    LiteralData, String,
-    AllowedRange, UnitLinear,
+    LiteralData, ComplexData, FormatText, CDFileWrapper,
 )
-from eoxserver.backends.access import connect
-from vires import models
-from vires.util import get_total_seconds
 from vires.aux import query_dst, query_kp
-from vires.time_util import mjd2000_to_datetime
+from vires.time_util import (
+    mjd2000_to_datetime, mjd2000_to_unix_epoch, naive_to_utc,
+)
 
 
-class get_indices(Component):
-    """ Process to retrieve registered auxiliary data
+def abs_amax(arr, axis):
+    """ Get the elements with largest absolute values along the given axis."""
+    arr_min = amin(arr, axis)
+    arr_max = amax(arr, axis)
+    return choose(abs(arr_max) > abs(arr_min), (arr_min, arr_max))
+
+
+# Auxiliary data query function and file sources
+AUX_INDEX = {
+    "kp": (query_kp, settings.VIRES_AUX_DB_KP, amax, "%d"),
+    "dst": (query_dst, settings.VIRES_AUX_DB_DST, abs_amax, "%.6g"),
+}
+
+
+class GetIndices(Component):
+    """Retrieve auxiliary indices within the given time interval.
+    Empty response is returned if there is no value matched.
     """
     implements(ProcessInterface)
 
     identifier = "get_indices"
-    title = "Retrieve registered auxiliary indices based on start and end time"
-    metadata = {"test-metadata":"http://www.metadata.com/test-metadata"}
-    profiles = ["test_profile"]
+    title = "Auxiliary index retrieval."
+    metadata = {}
+    profiles = ["vires"]
 
     inputs = [
-        ("index_id", LiteralData('index_id', str, optional=False,
-            abstract="Index id from which auxiliary data will be retrieved",
-            allowed_values=('kp','dst'),
+        ("index_id", LiteralData(
+            'index_id', str, optional=False,
+            abstract="Identifier of the queried auxiliary index.",
+            allowed_values=('kp', 'dst'),
         )),
-        ("begin_time", LiteralData('begin_time', dt.datetime, optional=False,
-            abstract="Start of the time interval",
+        ("begin_time", LiteralData(
+            'begin_time', datetime, optional=False,
+            abstract="Start of the requested time interval",
         )),
-        ("end_time", LiteralData('end_time', dt.datetime, optional=False,
-            abstract="End of the time interval",
+        ("end_time", LiteralData(
+            'end_time', datetime, optional=False,
+            abstract="End of the requested time interval",
+        )),
+        ("csv_time_format", LiteralData(
+            'csv_time_format', str, optional=True, title="CSV time  format",
+            abstract="Optional time format used by the CSV output.",
+            allowed_values=("ISO date-time", "MJD2000", "Unix epoch"),
+            default="ISO date-time",
         )),
     ]
-
 
     outputs = [
-        ("output",
-            ComplexData('output',
-                title="Requested subset of data",
-                abstract="Process returns subset of data defined by time, bbox and collections.",
-                formats=FormatText('text/plain')
-            )
-        ),
+        ("output", ComplexData(
+            'output', title="Requested subset of data", abstract=(
+                "Process returns subset of data defined by time, bbox "
+                "and collections."
+            ), formats=(FormatText('text/csv'), FormatText('text/plain'))
+        )),
     ]
 
-    def execute(self, index_id, begin_time, end_time, **kwarg):
-        outputs = {}
+    def execute(self, index_id, begin_time, end_time, csv_time_format,
+                output, **kwarg):
+        # fix the time-zone of the naive date-time
+        begin_time = naive_to_utc(begin_time)
+        end_time = naive_to_utc(end_time)
 
-        output_data = OrderedDict()
+        query, filename, lessen, data_format = AUX_INDEX[index_id]
+        aux_data = query(filename, begin_time, end_time)
 
-        f = StringIO()
-        writer = csv.writer(f)
+        time = aux_data["time"]
+        data = aux_data[index_id]
 
-        writer.writerow(["id","value", "time"])
+        if time.size > 500:
+            bin_size = (time.size - 1)//500 + 1
+            bin_count = time.size // bin_size
+            size = bin_size * bin_count
+            shape = (bin_count, bin_size)
+            data = lessen(data[:size].reshape(shape), 1)
+            time = time[:size].reshape(shape)
+            time = 0.5 * (time[:, 0] + time[:, -1])
 
-        if index_id == "dst":
-            aux_data = query_dst(
-                settings.VIRES_AUX_DB_DST, begin_time, end_time
-            )
-        elif index_id == "kp":
-            aux_data = query_kp(
-                settings.VIRES_AUX_DB_KP, begin_time, end_time
-            )
-        #elif index_id == "ibia":
-        #    aux_data = aux.query_ibi_ni(
-        #        "A", begin_time, end_time
-        #    )
-        #    index_id = "bubble_index"
+        if csv_time_format == "ISO date-time":
+            time_format = "%s"
+            time = vectorize(mjd2000_to_datetime)(time)
+        elif csv_time_format == "Unix epoch":
+            time_format = "%.14g"
+            time = mjd2000_to_unix_epoch(time)
+        else: # csv_time_format == "MJD2000"
+            time_format = "%.14g"
 
-        t_arr = np.array(aux_data["time"]) 
-        v_arr = np.array(aux_data[index_id])
+        line_format = "%s,%s,%s\r\n"
+        row_format = line_format % (index_id, data_format, time_format)
 
-        if t_arr.size > 500:
+        output_fobj = StringIO()
+        output_fobj.write(line_format % ("id", "value", "time"))
+        for row in izip(data, time):
+            output_fobj.write(row_format % row)
 
-            bin_size = (t_arr.size - 1)/500 + 1 
-            bin_count = t_arr.size / bin_size
-            size_short = bin_size * bin_count
-            axis = 1
-
-            t_arr_new = np.average(t_arr[:size_short].reshape((bin_count, bin_size)), axis)
-
-            v_arr_max = np.amax(v_arr[:size_short].reshape((bin_count, bin_size)), axis)
-            v_arr_min = np.amin(v_arr[:size_short].reshape((bin_count, bin_size)), axis)
-
-            t_arr = t_arr_new
-            v_arr = [ v_max if abs(v_max) > abs(v_min) else v_min for v_max, v_min in izip(v_arr_max, v_arr_min) ]
-
-        t_arr = map(mjd2000_to_datetime, t_arr)
-
-        for row in izip(v_arr, t_arr):
-            writer.writerow([index_id] + map(translate, row))
-
-        outputs['output'] = f
-        
-        return outputs
-
-
-def translate(arr):
-    try:
-        if arr.ndim == 1:
-            return "{%s}" % ";".join(map(str, arr))
-    except:
-        pass
-
-    return arr
+        return CDFileWrapper(output_fobj, **output)
