56bd47e90cce79e3f1100f9520c194ccce8f1c8b
Martin Paces <martin.paces@eox.at>
2016-04-08 08:48:15 +0200

    Adding forward model retrieval convenience utility.

---
+++ vires/vires/forward_models/util.py
@@ -0,0 +1,45 @@
+#-------------------------------------------------------------------------------
+#
+# Forward Model utilities
+#
+# Project: VirES
+# Authors: Martin Paces <martin.paces@eox.at>
+#
+#-------------------------------------------------------------------------------
+# Copyright (C) 2016 EOX IT Services GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+# pylint: disable=too-few-public-methods
+
+from collections import OrderedDict
+from eoxserver.core import env, Component, ExtensionPoint
+from vires.interfaces import ForwardModelProviderInterface
+
+class ForwardModelProviders(Component):
+    """ Component providing list of forward models through the extension point.
+    """
+    forward_models = ExtensionPoint(ForwardModelProviderInterface)
+
+def get_forward_model_providers():
+    """ Get list of model providers. """
+    return OrderedDict(
+        (item.identifier, item)
+        for item in ForwardModelProviders(env).forward_models
+    )
