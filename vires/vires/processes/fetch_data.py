8e1ec66c259d30d7222f0f3d6d384a098e674160
Martin Paces <martin.paces@eox.at>
2016-09-21 16:55:18 +0200

    Fixing time-stamp format output.

--- vires/vires/processes/fetch_data.py
+++ vires/vires/processes/fetch_data.py
@@ -261,12 +261,18 @@ class FetchData(WPSProcess):
         for label, dataset in _generate_data_():
 
             # time-format conversion
-            for variable, data in dataset.iteritems():
-                cdf_type = dataset.cdf_type.get(variable)
-                if cdf_type == CDF_EPOCH_TYPE:
-                    dataset[variable] = CDF_RAW_TIME_CONVERTOR[csv_time_format](
-                        data, cdf_type
-                    )
+#            for variable, data in dataset.iteritems():
+#                cdf_type = dataset.cdf_type.get(variable)
+#                if cdf_type == CDF_EPOCH_TYPE:
+#                    dataset[variable] = CDF_RAW_TIME_CONVERTOR[csv_time_format](
+#                        data, cdf_type
+#                    )
+            variable = 'Timestamp'
+            data = dataset[variable]
+            dataset.cdf_type[variable] = CDF_EPOCH_TYPE
+            dataset[variable] = CDF_RAW_TIME_CONVERTOR[csv_time_format](
+                data, CDF_EPOCH_TYPE
+            )
 
             if initialize:
                 output_fobj.write("id,")
