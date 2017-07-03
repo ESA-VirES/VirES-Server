f3d7db5770b34afc7ee91eba02e46d4f123e4827
Martin Paces <martin.paces@eox.at>
2017-07-03 08:41:49 +0200

    Adding missing module.

---
+++ vires/vires/processes/util/variables.py
@@ -0,0 +1,145 @@
+#-------------------------------------------------------------------------------
+#
+# Variable Dependency Resolver
+#
+# Project: VirES
+# Authors: Martin Paces <martin.paces@eox.at>
+#
+#-------------------------------------------------------------------------------
+# Copyright (C) 2017 EOX IT Services GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+# pylint: disable=too-many-arguments, too-many-locals, too-many-branches
+
+from itertools import chain
+from vires.util import unique, include, exclude
+from .filters import RejectAll
+
+
+class VariableResolver(object):
+    """ Variable Resolver collects the available sources, models and filters
+    and resolves the variable dependencies.
+
+    Variables types:
+        output:    list of all output variables.
+        available: list of all available variables which can be used
+                   in the output.
+        requested: optional list of variables requested by the user
+                   to be in the output. If not set all available variables
+                   are used.
+        mandatory: optional list of variables desired always in the output
+                   even if not explicitly requested.
+        required:  variables which are required to be available in order
+                   to evaluate the outputs
+    """
+
+    def __init__(self, requested=None, mandatory=None, required=None):
+        self.requested = tuple(unique(requested)) if requested else ()
+        self.mandatory = tuple(unique(mandatory)) if mandatory else ()
+        self._available_list = []
+        self._available_set = set()
+        self._required_set = set(required if required else ())
+
+        self.master = None
+        self.slaves = []
+        self.models = []
+        self._resolved_filters = []
+        self._unresolved_filters = []
+
+    @property
+    def available(self):
+        """ Get tuple of available variables. """
+        return tuple(unique(self._available_list))
+
+    @property
+    def output(self):
+        """ Get tuple of output variables. """
+        available = self.available
+        output = (
+            include(self.requested, available) if self.requested else available
+        )
+        return tuple(chain(
+            include(self.mandatory, available), # mandatory first
+            exclude(output, self.mandatory)
+        ))
+
+    @property
+    def required(self):
+        """ Get tuple of variables required to be evaluated. """
+        return tuple(unique(chain(self.output, self._required_set)))
+
+    @property
+    def unresolved_filters(self):
+        """ Get list of unresolved filters. """
+        return tuple(self._unresolved_filters)
+
+    @property
+    def resolved_filters(self):
+        """ Get list of applicable resolved filters. """
+        return tuple(self._resolved_filters)
+
+    @property
+    def filters(self):
+        """ Get list of filters to be applied to the data."""
+        if self._unresolved_filters:
+            return (RejectAll(),)
+        else:
+            return self.resolved_filters
+
+    def add_master(self, master):
+        """ Add the master source. Only one master allowed.
+        The master source does not require any variables and contribute solely
+        to the available available variables.
+        """
+        if self.master:
+            raise RuntimeError("Master is already set!")
+        self._available_set.update(master.variables)
+        self._available_list.extend(master.variables)
+        self.master = master
+
+    def add_slave(self, slave, matching_variable):
+        """ Add a new slave source. Any number of slaves is allowed.
+        The slave source requires only the matching variables to be available.
+        """
+        if matching_variable in self._available_set:
+            self._required_set.add(matching_variable)
+            self._available_list.extend(slave.variables)
+            self._available_set.update(slave.variables)
+            self.slaves.append(slave)
+
+    def add_model(self, model):
+        """ Add derived model variables.
+        The derived variables require certain variables to be present
+        in order to be evaluated.
+        """
+        if not set(model.required_variables) - self._available_set:
+            self._required_set.update(model.required_variables)
+            self._available_list.extend(model.variables)
+            self._available_set.update(model.variables)
+            self.models.append(model)
+
+    def add_filter(self, filter_):
+        """ Add new filter and check whether its dependencies are satisfied.
+        """
+        if not set(filter_.required_variables) - self._available_set:
+            self._required_set.update(filter_.required_variables)
+            self._resolved_filters.append(filter_)
+        else:
+            self._unresolved_filters.append(filter_)
