35955bd99fe9400dd456c54776e1f2cca67df23a
Martin Paces <martin.paces@eox.at>
2018-04-26 16:39:53 +0200

    Enabling the magnetic model to correctly handle MIO models.

--- vires/vires/processes/util/model_magmod.py
+++ vires/vires/processes/util/model_magmod.py
@@ -29,9 +29,10 @@
 #pylint: disable=too-many-locals
 
 from logging import getLogger, LoggerAdapter
+from itertools import chain
 from numpy import stack
 from eoxmagmod import vnorm
-from vires.util import include, unique
+from vires.util import include, unique, cached_property
 from vires.cdf_util import cdf_rawtime_to_mjd2000, CDF_DOUBLE_TYPE
 from vires.dataset import Dataset
 from .model import Model
@@ -40,44 +41,42 @@ from .model import Model
 class MagneticModelResidual(Model):
     """ Residual evaluation. """
 
+    @cached_property
+    def variables(self):
+        return ["%s_res_%s" % (self.variable, self.model_name)]
+
+    @cached_property
+    def required_variables(self):
+        return [self.variable, "%s_%s" % (self.variable, self.model_name)]
+
     class _LoggerAdapter(LoggerAdapter):
         def process(self, msg, kwargs):
             return '%s: %s' % (self.extra["residual_name"], msg), kwargs
 
     def __init__(self, model_name, variable, logger=None):
         self.model_name = model_name
-        self._required_variables = [variable, "%s_%s" % (variable, model_name)]
         self.variable = variable
-        self.model_variable = "%s_%s" % (variable, model_name)
-        self.output_variable = "%s_res_%s" % (variable, model_name)
         self.logger = self._LoggerAdapter(logger or getLogger(__name__), {
-            "residual_name": self.output_variable,
+            "residual_name": self.variables[0],
         })
-
-    @property
-    def variables(self):
-        return [self.output_variable]
-
-    @property
-    def required_variables(self):
-        return [self.variable, self.model_variable]
 
     def eval(self, dataset, variables=None, **kwargs):
         output_ds = Dataset()
-        proceed = variables is None or self.output_variable in variables
+        output_variable, = self.variables
+        is_requested = variables is None or output_variable in variables
         self.logger.debug(
-            "requested variables %s", self.variables if proceed else []
+            "requested variables %s", self.variables if is_requested else []
         )
-        if proceed:
+        if is_requested:
             self.logger.debug("requested dataset length %s", dataset.length)
+            measurement_variable, model_variable = self.required_variables
             output_ds.set(
-                self.output_variable,
-                dataset[self.variable] - dataset[self.model_variable],
-                dataset.cdf_type[self.variable],
-                self._get_attributes(dataset, self.variable),
+                output_variable,
+                dataset[measurement_variable] - dataset[model_variable],
+                dataset.cdf_type[measurement_variable],
+                self._get_attributes(dataset, measurement_variable),
             )
         return output_ds
-
 
     def _get_attributes(self, dataset, variable):
         src_attr = dataset.cdf_attr.get(variable)
@@ -101,10 +100,26 @@ class MagneticModelResidual(Model):
 
 class MagneticModel(Model):
     """ Forward spherical expansion model. """
-    DEFAULT_REQUIRED_VARIABLES = [
-        "Timestamp", "Latitude", "Longitude", "Radius",
-    ]
+    SOURCE_VARIABLES = {
+        "time": ["Timestamp"],
+        "location": ["Latitude", "Longitude", "Radius"],
+        "f107": ["F10_INDEX"],
+        "subsolar_point": ["SunDeclination", "SunLongitude"]
+    }
     BASE_VARIABLES = ["F", "B_NEC"]
+
+    @cached_property
+    def variables(self):
+        model_name = self.name
+        return [
+            "%s_%s" % (variable, model_name) for variable in self.BASE_VARIABLES
+        ]
+
+    @cached_property
+    def required_variables(self):
+        return list(chain.from_iterable(
+            variables for variables in self._source_variables.values()
+        ))
 
     class _LoggerAdapter(LoggerAdapter):
         def process(self, msg, kwargs):
@@ -114,51 +129,30 @@ class MagneticModel(Model):
         self.name = model_name
         self.model = model
         varmap = varmap or {}
-        self._required_variables = [
-            varmap.get(var, var) for var in self.DEFAULT_REQUIRED_VARIABLES
+
+        available_data_extractors = {
+            "time": self._extract_time,
+            "location": self._extract_location,
+            "f107": self._extract_f107,
+            "subsolar_point": self._extract_subsolar_point,
+        }
+
+        self.data_extractors = [
+            available_data_extractors[requirement]
+            for requirement in model.parameters
         ]
-        self._provided_variables = [
-            "%s_%s" % (variable, model_name) for variable in self.BASE_VARIABLES
-        ]
+
+        self._source_variables = {
+            requirement: [
+                varmap.get(var, var) for var
+                in self.SOURCE_VARIABLES[requirement]
+            ]
+            for requirement in model.parameters
+        }
+
         self.logger = self._LoggerAdapter(logger or getLogger(__name__), {
             "model_name": model_name,
         })
-        self.attrib = self._variable_attributes()
-
-    def _variable_attributes(self):
-        f_var, b_var = self.variables
-        return {
-            f_var: {
-                'DESCRIPTION': (
-                    'Magnetic field intensity, calculated by '
-                    'the %s spherical harmonic model' % self.name
-                ),
-                'UNITS': 'nT',
-            },
-            b_var: {
-                'DESCRIPTION': (
-                    'Magnetic field vector, NEC frame, calculated by '
-                    'the %s spherical harmonic model' % self.name
-                ),
-                'UNITS': 'nT',
-            },
-        }
-
-    @property
-    def variables(self):
-        return self._provided_variables
-
-    @property
-    def required_variables(self):
-        return self._required_variables
-
-    def _extract_required_variables(self, dataset):
-        time, latitude, longitude, radius = self._required_variables
-        # Note: radius is converted from metres to kilometres
-        return (
-            cdf_rawtime_to_mjd2000(dataset[time], dataset.cdf_type[time]),
-            dataset[latitude], dataset[longitude], 1e-3*dataset[radius],
-        )
 
     def eval(self, dataset, variables=None, **kwargs):
         variables = (
@@ -169,20 +163,64 @@ class MagneticModel(Model):
         output_ds = Dataset()
 
         if variables:
-            times, lats, lons, rads = self._extract_required_variables(dataset)
+            inputs = {"scale": [1, 1, -1]}
+            for extract in self.data_extractors:
+                inputs.update(extract(dataset))
 
-            result = self.model.eval(
-                times, stack((lats, lons, rads), axis=1), scale=[1, 1, -1]
-            )
+            result = self.model.eval(**inputs)
 
-            f_var, b_var = self.variables
-            if f_var in variables:
-                output_ds.set(
-                    f_var, vnorm(result), CDF_DOUBLE_TYPE, self.attrib[f_var]
-                )
-            if b_var in variables:
-                output_ds.set(
-                    b_var, result, CDF_DOUBLE_TYPE, self.attrib[b_var]
-                )
+            for variable in variables:
+                filter_, type_, attrib = self.output[variable]
+                output_ds.set(variable, filter_(result), type_, attrib)
 
         return output_ds
+
+    @cached_property
+    def output(self):
+        f_var, b_var = self.variables
+        return {
+            f_var: (vnorm, CDF_DOUBLE_TYPE, {
+                'DESCRIPTION': (
+                    'Magnetic field intensity, calculated by '
+                    'the %s spherical harmonic model' % self.name
+                ),
+                'UNITS': 'nT',
+            }),
+            b_var: (lambda r: r, CDF_DOUBLE_TYPE, {
+                'DESCRIPTION': (
+                    'Magnetic field vector, NEC frame, calculated by '
+                    'the %s spherical harmonic model' % self.name
+                ),
+                'UNITS': 'nT',
+            }),
+        }
+
+    def _extract_time(self, dataset):
+        time, = self._source_variables["time"]
+        return [
+            ("time", cdf_rawtime_to_mjd2000(
+                dataset[time], dataset.cdf_type[time]
+            )),
+        ]
+
+    def _extract_location(self, dataset):
+        latitude, longitude, radius = self._source_variables["location"]
+        return [
+            ("location", stack((
+                # Note: radius is converted from metres to kilometres
+                dataset[latitude], dataset[longitude], 1e-3*dataset[radius],
+            ), axis=1)),
+        ]
+
+    def _extract_f107(self, dataset):
+        f107, = self._source_variables["f107"]
+        return [
+            ("f107", dataset[f107]),
+        ]
+
+    def _extract_subsolar_point(self, dataset):
+        lat_sol, lon_sol = self._source_variables["subsolar_point"]
+        return [
+            ("lat_sol", dataset[lat_sol]),
+            ("lon_sol", dataset[lon_sol]),
+        ]
