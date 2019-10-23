a00a4a8fbe8dcf9af8333c52fb30e10873adcb23
Martin Paces <martin.paces@eox.at>
2019-10-23 10:19:45 +0200

    Fixing the tracking initialization.
    Accepting the tracking cookies starts the actual tracking.

--- eoxs_allauth/eoxs_allauth/static/js/cookie_banner.js
+++ eoxs_allauth/eoxs_allauth/static/js/cookie_banner.js
@@ -109,19 +109,3 @@ var CookieBanner = (function() {
         }
     };
 })();
-
-window.onload = function(){
-    var html = '<div>' +
-        'You may choose to prevent this website from aggregating and analyzing '+
-        'the actions you take here. Doing so will protect your privacy, but '+
-        'will also prevent the service provider from learning from your actions '+
-        'and creating a better experience for you and other users.'+
-        'For further information see our <a href="/accounts/privacy_notice" target="_blank">Privacy Notice</a>.'+
-        '</div>';
-
-    // Add the accept button
-    html += '<div class="cookiebutton ok"><a href="javascript:void(0);" onclick="CookieBanner.accept();"><span>Accept</span></a></div>';
-    html += '<div class="cookiebutton notok"><a href="javascript:void(0);" onclick="CookieBanner.deny();"><span>Decline</span></a></div>';
-
-    CookieBanner.showUnlessInteracted(html);
-}
