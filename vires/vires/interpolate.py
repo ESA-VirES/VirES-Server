0eb1c1a0a510ef2d938da7f82a129b38fd73069f
Martin Paces <martin.paces@eox.at>
2020-05-27 12:10:22 +0200

    Refactoring gap-aware 1D interpolation + adding linear interpolator.

--- vires/vires/interpolate.py
+++ vires/vires/interpolate.py
@@ -24,12 +24,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 #-------------------------------------------------------------------------------
-# pylint: disable=too-many-instance-attributes,too-many-arguments,too-many-locals
+## pylint: disable=too-few-public-methods,too-many-arguments,too-many-locals
 
 from logging import getLogger
 from numpy import (
-    array, arange, empty, isnan, invert, diff, inf, nan, concatenate,
-    searchsorted, full,
+    arange, empty, isnan, invert, diff, inf, nan, concatenate,
+    searchsorted, full, floor, clip, stack, expand_dims,
 )
 import scipy
 from scipy.interpolate import interp1d
@@ -38,31 +38,24 @@ from scipy.interpolate import interp1d
 class Interp1D():
     """ 1D interpolator.
     This class is optimised for repeated 1D interpolation of multiple
-    dataset using the same target sampling (x_dst).
-    NOTE: x_src and x_dst must be sorted in ascending order.
+    datasets using the same target sampling (x_dst).
+    NOTE: both x_src and x_dst must be sorted in ascending order.
     """
 
     def __init__(self, x_src, x_dst, gap_threshold=inf,
                  segment_neighbourhood=0, logger=None):
-        """ Initialize interpolator. """
+        self.logger = logger or getLogger(__name__)
         self.x_src = x_src
         self.x_dst = x_dst
-        self.gap_threshold = gap_threshold
+        self.gap_threshold = max(0, gap_threshold)
         self.segment_neighbourhood = (
             max(0, min(gap_threshold, segment_neighbourhood))
         )
-        self.prm = {
-            "bounds_error": False,
-            "copy": False,
-        }
-        if scipy.__version__ >= '0.14':
-            self.prm['assume_sorted'] = True
-        self._indices_nearest = None
-        self._data_zero = None
-        self.logger = logger or getLogger(__name__)
-        self._supported_kinds = {
-            "nearest": self._nearest,
-            "zero": self._zero,
+        self._interpolators = {}
+        self._iterpolator_classes = {
+            'nearest': NearestNeighbour1DInterpolator,
+            'zero': PreviousNeighbour1DInterpolator,
+            'linear': Linear1DInterpolator,
         }
 
     def __call__(self, y_src, kind):
@@ -74,115 +67,239 @@ class Interp1D():
                 "interpolation axis."
             )
 
-        try:
-            handler = self._supported_kinds[kind]
-        except KeyError:
-            raise ValueError("Invalid interpolation kind %r!" % kind)
+        iterpolator = self._interpolators.get(kind)
 
-        return handler(y_src)
+        if not iterpolator:
+            try:
+                iterpolator_class = self._iterpolator_classes[kind]
+            except KeyError:
+                raise ValueError("Invalid interpolation kind %r!" % kind)
 
-    @property
-    def indices_nearest(self):
-        """ Get indices of the nearest neighbour  interpolation. """
-        if not self._indices_nearest:
-            self._init_nearest()
-        return self._indices_nearest
+            self._interpolators[kind] = iterpolator = iterpolator_class(
+                self.x_src, self.x_dst,
+                self.gap_threshold,
+                self.segment_neighbourhood,
+            )
 
-    @property
-    def data_zero(self):
-        """ Get indices of the zero-order interpolation. """
-        if not self._data_zero:
-            self._init_zero()
-        return self._data_zero
+        return iterpolator(y_src)
 
-    def _nearest(self, y_src):
-        """ Nearest neighbour interpolation. """
-        return self._select_values(y_src, *self.indices_nearest)
 
-    def _zero(self, y_src):
-        """ Zero-kind interpolation. """
-        return self._select_values(y_src, *self.data_zero)
+class BaseNeighbour1DInterpolator():
+    """ Base neighbour 1D interpolator. """
 
-    def _select_values(self, y_src, idx_nan, idx_valid, idx_source):
-        """ Select the interpolated values. """
-        y_dst = empty((len(self.x_dst),) + y_src.shape[1:])
+    def __init__(self, x_src, x_dst, gap_threshold,
+                 lower_neighbourhood, upper_neighbourhood):
+        self.x_dst_shape = x_dst.shape
+        self.idx_nan, self.idx_valid, self.index = self._get_indices(
+            x_src, x_dst,
+            gap_threshold=max(0, gap_threshold),
+            lower_neighbourhood=max(0, lower_neighbourhood),
+            upper_neighbourhood=max(0, upper_neighbourhood),
+        )
+
+    def __call__(self, y_src):
+        y_dst = empty(self.x_dst_shape[:1] + y_src.shape[1:])
         if y_dst.size > 0:
-            y_dst[idx_nan] = nan
-            # NOTE: workaround for the empty multi-dimensional slicing bug
-            #       in NumPy v1.7.1
-            if idx_valid.size > 0:
-                y_dst[idx_valid] = y_src[idx_source]
+            y_dst[self.idx_nan] = nan
+            y_dst[self.idx_valid] = y_src[self.index]
         return y_dst
 
-    def _init_nearest(self):
-        """ Initialize the nearest neighbour interpolation. """
-        self._indices_nearest = self._get_value_indices(
-            "nearest", self.segment_neighbourhood, self.segment_neighbourhood
+    def _find_indices(self, x_in, i_in, x_out):
+        raise NotImplementedError
+
+    def _get_indices(self, x_src, x_dst, gap_threshold,
+                     lower_neighbourhood, upper_neighbourhood):
+        """ Get valid/invalid values mask and mapping of the source
+        to the destination values.
+        """
+        segments = _generate_contigous_segments(
+            x_src, x_dst, gap_threshold,
+            lower_neighbourhood, upper_neighbourhood,
         )
 
-    def _init_zero(self):
-        """ Initialize the zero-kind interpolation. """
-        self._data_zero = self._get_value_indices(
-            "zero", 0, self.segment_neighbourhood
-        )
+        index = full(x_dst.shape, nan)
+        for segment in segments:
+            (
+                (x_l, x_h),
+                (idx_src_low, idx_src_high),
+                (idx_dst_low, idx_dst_high),
+            ) = segment
 
-    def _get_value_indices(self, kind, lower_neighbourhood, upper_neighbourhood):
-        self.logger.debug("%s: x_src shape: %s", kind, self.x_src.shape)
-        self.logger.debug("%s: x_dst shape: %s", kind, self.x_dst.shape)
-        self.logger.debug("%s: gap_threshold: %s", kind, self.gap_threshold)
-        self.logger.debug(
-            "%s: lower_neighbourhood: %s", kind, lower_neighbourhood
-        )
-        self.logger.debug(
-            "%s: upper_neighbourhood: %s", kind, upper_neighbourhood
-        )
+            index[idx_dst_low:idx_dst_high] = self._find_indices(
+                concatenate(
+                    ([x_l], x_src[idx_src_low:idx_src_high], [x_h])
+                ),
+                concatenate(
+                    ([idx_src_low], arange(idx_src_low, idx_src_high), [idx_src_high-1])
+                ),
+                x_dst[idx_dst_low:idx_dst_high]
+            )
 
-        index = full(self.x_dst.shape, nan)
-        # iterate over the contiguous value ranges
-        ranges = self._contiguous_ranges(self.x_src, self.gap_threshold)
-        for low, high in ranges:
-            self.logger.debug("%s: contiguous range: %d:%d", kind, low, high)
-            x_l = self.x_src[low] - max(0.0, lower_neighbourhood)
-            x_h = self.x_src[high-1] + max(0.0, upper_neighbourhood)
-            if high - low == 1:
-                # single value segment
-                x_src = array([x_l, x_h])
-                i_src = array([low, low])
-            else:
-                # regular multi-value segment
-                x_src = self.x_src[low:high]
-                i_src = arange(low, high)
-                x_src = concatenate(([x_l], x_src, [x_h]))
-                i_src = concatenate(([low], i_src, [high-1]))
-            # get range of the data overlapping the interpolated segment
-            idx_dst_low = searchsorted(self.x_dst, x_l, side='left')
-            idx_dst_high = searchsorted(self.x_dst, x_h, side='right')
-            # segment interpolation
-            if idx_dst_high > idx_dst_low:
-                index[idx_dst_low:idx_dst_high] = interp1d(
-                    x_src, i_src, kind=kind, **self.prm
-                )(self.x_dst[idx_dst_low:idx_dst_high])
-        # extract index mapping
-        is_nan = isnan(index)
-        idx_nan = is_nan.nonzero()[0]
-        idx_valid = invert(is_nan).nonzero()[0]
-        idx_source = index[idx_valid].astype('int')
+        idx_nan, idx_valid = _get_nan_mask(index)
+        index_source = index[idx_valid].astype('int')
 
-        self.logger.debug(
-            "%s: %d mapped, %d invalids", kind, len(idx_valid), len(idx_nan)
+        return idx_nan, idx_valid, index_source
+
+
+class NearestNeighbour1DInterpolator(BaseNeighbour1DInterpolator):
+    """ Nearest neighbour 1D interpolator. """
+
+    def __init__(self, x_src, x_dst, gap_threshold=inf, segment_neighbourhood=0):
+        super().__init__(
+            x_src=x_src,
+            x_dst=x_dst,
+            gap_threshold=gap_threshold,
+            lower_neighbourhood=segment_neighbourhood,
+            upper_neighbourhood=segment_neighbourhood,
         )
-
-        return idx_nan, idx_valid, idx_source
 
     @staticmethod
-    def _contiguous_ranges(x_src, gap_threshold):
-        """ Generator returning contiguous ranges of the interpolated
-        variable.
+    def _find_indices(x_in, i_in, x_out):
+        options = {
+            "kind": "nearest",
+            "bounds_error": False,
+            "copy": False,
+        }
+        if scipy.__version__ >= '0.14':
+            options['assume_sorted'] = True
+        return interp1d(x_in, i_in, **options)(x_out)
+
+
+class PreviousNeighbour1DInterpolator(BaseNeighbour1DInterpolator):
+    """ Previous neighbour 1D interpolator. """
+
+    def __init__(self, x_src, x_dst, gap_threshold=inf, segment_neighbourhood=0):
+        super().__init__(
+            x_src=x_src,
+            x_dst=x_dst,
+            gap_threshold=gap_threshold,
+            lower_neighbourhood=0,
+            upper_neighbourhood=segment_neighbourhood,
+        )
+
+    @staticmethod
+    def _find_indices(x_in, i_in, x_out):
+        options = {
+            "kind": "zero",
+            "bounds_error": False,
+            "copy": False,
+        }
+        if scipy.__version__ >= '0.14':
+            options['assume_sorted'] = True
+        return interp1d(x_in, i_in, **options)(x_out)
+
+
+class Linear1DInterpolator():
+    """ Linear 1D interpolator. """
+
+    def __init__(self, x_src, x_dst, gap_threshold=inf, segment_neighbourhood=0):
+        gap_threshold = max(0, gap_threshold)
+        segment_neighbourhood = max(0, segment_neighbourhood)
+        self.x_dst_shape = x_dst.shape
+        self.idx_nan, self.idx_valid, self.index, self.base = self._get_indices(
+            x_src, x_dst, gap_threshold,
+            segment_neighbourhood, segment_neighbourhood,
+        )
+
+    def __call__(self, y_src):
+        y_dst = empty(self.x_dst_shape[:1] + y_src.shape[1:])
+        if y_dst.size > 0:
+            y_dst[self.idx_nan] = nan
+            # expand_dim requires NumPy >= v1.8.0
+            extra_dims = tuple(range(2, y_src.ndim + 1))
+            y_dst[self.idx_valid] = (
+                expand_dims(self.base, axis=extra_dims) * y_src[self.index]
+            ).sum(axis=1)
+        return y_dst
+
+    @staticmethod
+    def _find_indices(x_in, i_in, x_out):
+        options = {
+            "kind": "linear",
+            "fill_value": "extrapolate",
+            "bounds_error": False,
+            "copy": False,
+        }
+        if scipy.__version__ >= '0.14':
+            options['assume_sorted'] = True
+        return interp1d(x_in, i_in, **options)(x_out)
+
+    def _get_indices(self, x_src, x_dst, gap_threshold,
+                     lower_neighbourhood, upper_neighbourhood):
+        """ Get valid/invalid values mask and mapping of the source
+        to the destination values.
         """
-        idx_low = 0
-        for idx_high in (diff(x_src) > gap_threshold).nonzero()[0] + 1:
-            yield idx_low, idx_high
-            idx_low = idx_high
-        idx_high = len(x_src)
-        if idx_high > 0:
-            yield idx_low, idx_high
+        segments = _generate_contigous_segments(
+            x_src, x_dst, gap_threshold,
+            lower_neighbourhood, upper_neighbourhood,
+        )
+
+        index = full(x_dst.shape, -1)
+        base = full(x_dst.shape, nan)
+        for segment in segments:
+            (
+                _,
+                (idx_src_low, idx_src_high),
+                (idx_dst_low, idx_dst_high),
+            ) = segment
+
+            if idx_src_high - 2 < idx_src_low:
+                continue # skip single value segments
+
+            tmp0 = self._find_indices(
+                x_src[idx_src_low:idx_src_high],
+                arange(idx_src_low, idx_src_high),
+                x_dst[idx_dst_low:idx_dst_high]
+            )
+
+            tmp1 = clip(floor(tmp0), idx_src_low, idx_src_high - 2)
+
+            index[idx_dst_low:idx_dst_high] = tmp1
+            base[idx_dst_low:idx_dst_high] = tmp0 - tmp1
+
+            del tmp0, tmp1
+
+        idx_nan, idx_valid = _get_nan_mask(base)
+
+        index = index[idx_valid]
+        base = base[idx_valid]
+
+        index = stack((index, index+1), axis=1)
+        base = stack((1.0 - base, base), axis=1)
+
+        return idx_nan, idx_valid, index, base
+
+
+def _generate_contigous_segments(x_src, x_dst, gap_threshold,
+                                 lower_neighbourhood, upper_neighbourhood):
+    """ Generator yielding contiguous data ranges. """
+    # process x_src contiguous ranges
+    for idx_src_low, idx_src_high in _generate_contiguous_ranges(x_src, gap_threshold):
+        x_l = x_src[idx_src_low] - lower_neighbourhood
+        x_h = x_src[idx_src_high-1] + upper_neighbourhood
+        # get range of the x_dst overlapping the x_src segment
+        idx_dst_low = searchsorted(x_dst, x_l, side='left')
+        idx_dst_high = searchsorted(x_dst, x_h, side='right')
+        if idx_dst_high > idx_dst_low:
+            yield (
+                (x_l, x_h),
+                (idx_src_low, idx_src_high),
+                (idx_dst_low, idx_dst_high),
+            )
+
+
+def _generate_contiguous_ranges(x_src, gap_threshold):
+    """ Generator yielding contiguous ranges of the interpolated variable. """
+    idx_low = 0
+    for idx_high in (diff(x_src) > gap_threshold).nonzero()[0] + 1:
+        yield idx_low, idx_high
+        idx_low = idx_high
+    idx_high = len(x_src)
+    if idx_high > 0:
+        yield idx_low, idx_high
+
+
+def _get_nan_mask(index):
+    """ Get indices of the valid and NaN values. """
+    is_nan = isnan(index)
+    return is_nan.nonzero()[0], invert(is_nan).nonzero()[0]
