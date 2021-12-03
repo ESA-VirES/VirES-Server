2fe6d44e7265a02601066500723dc55f530dd4b4
Martin Paces <martin.paces@eox.at>
2021-12-03 13:01:53 +0100

    EOIAM indentity provider

---
+++ vires_oauth/vires_oauth/providers/eoiam/provider.py
@@ -0,0 +1,71 @@
+#-------------------------------------------------------------------------------
+#
+#  ESA EOIAM provider - "social account" provider class
+#
+# Authors: Martin Paces <martin.paces@eox.at>
+#-------------------------------------------------------------------------------
+# Copyright (C) 2021 EOX IT Services GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+
+from allauth.socialaccount import app_settings
+from allauth.socialaccount.providers.base import ProviderAccount
+from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
+
+
+class EoiamAccount(ProviderAccount):
+
+    def get_profile_url(self):
+        return None
+        #return self.account.extra_data.get( ... TBD ... )
+
+    def get_avatar_url(self):
+        return None
+        #return self.account.extra_data.get( ... TBD ... )
+
+    def to_str(self):
+        dflt = super(EoiamAccount, self).to_str()
+        return self.account.extra_data.get("sub", dflt)
+
+
+class EoiamProvider(OAuth2Provider):
+    id = 'eoiam'
+    name = 'EOIAM'
+    account_class = EoiamAccount
+
+    @staticmethod
+    def get_default_scope():
+        return ["openid", "profile", "email"]
+
+    @staticmethod
+    def extract_uid(data):
+        return data['username']
+
+    @staticmethod
+    def extract_common_fields(data):
+        return {
+            'username': data['sub'],
+            'email': data['sub'],
+            'first_name': data.get('given_name'),
+            'last_name': data.get('family_name'),
+        }
+
+
+provider_classes = [EoiamProvider]
---
+++ vires_oauth/vires_oauth/providers/eoiam/views.py
@@ -0,0 +1,72 @@
+#-------------------------------------------------------------------------------
+#
+#  ESA EOIAM provider - views
+#
+# Authors: Martin Paces <martin.paces@eox.at>
+#-------------------------------------------------------------------------------
+# Copyright (C) 2021 EOX IT Services GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+# pylint: disable=missing-docstring
+
+#
+# Required settings:
+#
+# SOCIALACCOUNT_PROVIDERS = {
+#     'eoiam': {
+#         'SERVER_URL': <OAuth2 server URL>,
+#     },
+# }
+
+import requests
+
+from allauth.socialaccount import app_settings
+from allauth.socialaccount.providers.oauth2.views import (
+    OAuth2Adapter, OAuth2CallbackView, OAuth2LoginView,
+)
+from .provider import EoiamProvider
+
+
+class EoiamOAuth2Adapter(OAuth2Adapter):
+    provider_id = EoiamProvider.id
+    settings = app_settings.PROVIDERS.get(provider_id, {})
+
+    # URL used for browser-to-server connections
+    server_url = settings['SERVER_URL'].rstrip('/')
+
+    access_token_url = '{0}/token'.format(server_url)
+    authorize_url = '{0}/authorize'.format(server_url)
+    profile_url = '{0}/userinfo'.format(server_url)
+
+    @classmethod
+    def read_profile(cls, token):
+        headers = {'Authorization': 'Bearer %s' % token}
+        return requests.get(cls.profile_url, headers=headers)
+
+    def complete_login(self, request, app, token, **kwargs):
+        extra_data = self.read_profile(token.token).json()
+        return self.get_provider().sociallogin_from_response(request, extra_data)
+
+    def get_email(self, token):
+        raise NotImplementedError
+
+
+oauth2_login = OAuth2LoginView.adapter_view(EoiamOAuth2Adapter)
+oauth2_callback = OAuth2CallbackView.adapter_view(EoiamOAuth2Adapter)
