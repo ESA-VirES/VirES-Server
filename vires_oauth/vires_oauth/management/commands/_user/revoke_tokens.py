03a2ead435c61b94cc6bd31fa4e95c042f34f970
Martin Paces <martin.paces@eox.at>
2024-10-20 21:07:59 +0200

    Auth server: adding forced OAuth token revocation.

--- vires_oauth/vires_oauth/management/commands/_user/revoke_tokens.py
+++ vires_oauth/vires_oauth/management/commands/_user/revoke_tokens.py
@@ -1,10 +1,10 @@
 #-------------------------------------------------------------------------------
 #
-#  User management API
+# User management - force revocation of all valid tokens
 #
 # Authors: Martin Paces <martin.paces@eox.at>
 #-------------------------------------------------------------------------------
-# Copyright (C) 2021-2024 EOX IT Services GmbH
+# Copyright (C) 2024 EOX IT Services GmbH
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -24,8 +24,30 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 #-------------------------------------------------------------------------------
-# pylint: disable=missing-docstring
+# pylint: disable=missing-docstring, too-few-public-methods
 
-from .import_ import save_user
-from .export import serialize_user
-from .tokens import revoke_tokens, remove_tokens
+from django.contrib.auth.models import User
+from vires_oauth.management.api.user import remove_tokens, revoke_tokens
+from .common import UserSelectionSubcommandProtected
+
+
+class RevokeTokensSubcommand(UserSelectionSubcommandProtected):
+    name = "revoke_tokens"
+    help = "Force revocation of all valid OAuth/OIdC user tokens."
+
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        parser.add_argument(
+            "-d", "--delete", dest="remove_tokens", action="store_true",
+            default=False, help="Force removal of the revoked tokens."
+        )
+
+    def handle(self, **kwargs):
+        users = self.select_users(User.objects.all(), **kwargs)
+
+        _revoke_tokens = (
+            remove_tokens if kwargs["remove_tokens"] else revoke_tokens
+        )
+
+        for user in users:
+            _revoke_tokens(user, logger=self)
