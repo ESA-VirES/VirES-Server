54cb4aea19d4f8633cada760113e6c521fedbc71
Martin Paces <martin.paces@eox.at>
2026-09-08 22:20:08 +0200

    Product registration - testing invalid time extent

--- vires/vires/metadata/cdf/multitime.py
+++ vires/vires/metadata/cdf/multitime.py
@@ -1,11 +1,11 @@
 #-------------------------------------------------------------------------------
 #
-# Products metadata extraction - generic CDF metadata reader
+# Products metadata extraction - CDF metadata reader for datasets with multiple
+#                                timestamp variables
 #
-# Authors: Fabian Schindler <fabian.schindler@eox.at>
-#          Martin Paces <martin.paces@eox.at>
+# Authors: Martin Paces <martin.paces@eox.at>
 #-------------------------------------------------------------------------------
-# Copyright (C) 2014-2024 EOX IT Services GmbH
+# Copyright (C) 2025 EOX IT Services GmbH
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -34,31 +34,38 @@ from ...time_cdf import DT_INVALID_VALUE
 DT_INVALID_VALUE_UTC = naive_to_utc(DT_INVALID_VALUE)
 
 
-class GenericCdfMetadataReader(CDFMetadataReader):
-    """ Metadata reader for generic CDF files.
+class MultitimeCdfMetadataReader(CDFMetadataReader):
+    """ Metadata reader for CDF files with multiple timestamp variables
+    and no default/main dataset.
+    The time-extent of a product is determined by extracting extent
+    of each dataset time variable.
     Works for Swarm and Swarm-like products.
     """
-    TYPE = "CDF-generic"
-    TIME_EXTENT_ATTRIBUTE = "TIME_EXTENT"
+    TYPE = "CDF-multitime"
 
     @classmethod
     def extract_options(cls, product_type):
         """ find primary time variable """
-        dataset_definition = product_type.get_dataset_definition(None)
-        primary_time_variables = (
-            properties.get("source", name)
-            for name, properties in dataset_definition.items()
-            if properties.get("primaryTimestamp")
-        )
-        for time_variable in primary_time_variables:
-            return {"time_variable": time_variable}
+
+        def _get_primary_time_variables():
+            datasets = product_type.definition['datasets']
+            for dataset_name in datasets:
+                dataset_definition = product_type.get_dataset_definition(dataset_name)
+                for name, properties in dataset_definition.items():
+                    if properties.get("primaryTimestamp"):
+                        yield properties.get("source", name)
+
+        if time_variables := list(_get_primary_time_variables()):
+            return {"time_variables": time_variables}
+
         raise ValueError(
-            f"Failed to find primary time variable of {product_type.identifier}!"
+            f"Failed to find any primary time variable of {product_type.identifier}!"
         )
+
 
     @classmethod
     def read_cdf_metadata(cls, cdf, **options):
-        begin_time, end_time = cls.get_time_range(cdf, options["time_variable"])
+        begin_time, end_time = cls.get_max_time_range(cdf, options["time_variables"])
         return {
             "format": cls.TYPE,
             "begin_time": begin_time,
@@ -66,22 +73,32 @@ class GenericCdfMetadataReader(CDFMetadataReader):
         }
 
     @classmethod
-    def _read_time_extent(cls, attr):
-        attr._raw = True # pylint: disable=protected-access
-        cdf_type = attr.type(0)
-        begin_time, end_time = attr[0]
-        return (
-            cls._cdf_rawtime_to_datetime(begin_time, cdf_type),
-            cls._cdf_rawtime_to_datetime(end_time, cdf_type),
-        )
+    def get_max_time_range(cls, cdf, time_variables):
+
+        def _update_time(current_time, update_time, compare_times):
+            if current_time is None:
+                return update_time
+            if update_time is None:
+                return current_time
+            return compare_times(current_time, update_time)
+
+        begin_time, end_time = None, None
+
+        for variable in time_variables:
+            next_begin_time, next_end_time = cls.get_time_range(cdf, variable)
+            print(variable, next_begin_time, next_end_time)
+            begin_time = _update_time(begin_time, next_begin_time, min)
+            end_time = _update_time(end_time, next_end_time, max)
+
+        if begin_time is None or end_time is None:
+            raise ValueError("Failed to extract product time range!")
+
+        print(" <== ", begin_time, end_time)
+
+        return begin_time, end_time
 
     @classmethod
     def get_time_range(cls, cdf, time_variable):
-
-        try:
-            return cls._read_time_extent(cdf.attrs[cls.TIME_EXTENT_ATTRIBUTE])
-        except (KeyError, IndexError, ValueError):
-            pass # fallback to extraction from the Timestamp array
 
         try:
             times = cdf.raw_var(time_variable)
@@ -90,6 +107,9 @@ class GenericCdfMetadataReader(CDFMetadataReader):
 
         if len(times.shape) != 1:
             raise ValueError("Incorrect dimension of the time-stamp array!")
+
+        if times.shape[0] == 0:
+            return None, None
 
         start = cls._cdf_rawtime_to_datetime(times[0], times.type())
         end = cls._cdf_rawtime_to_datetime(times[-1], times.type())
